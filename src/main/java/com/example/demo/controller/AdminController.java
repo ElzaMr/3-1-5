9b779a01b1c1efebfab142b370211feca189a9c6
ElzaMr <elza-mr@mail.ru>
2023-07-23 22:06:09 +0300

    the final

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
-
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
