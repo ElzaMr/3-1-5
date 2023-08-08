e7cd9a12c88eec821caa3b76c9ae0d3534694a65
ElzaMr <elza-mr@mail.ru>
2023-08-08 16:25:24 +0300

    мы все сломали

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,40 +1,40 @@
-package com.example.demo.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import com.example.demo.model.User;
-import com.example.demo.service.RoleService;
-import com.example.demo.service.UserService;
-
-import java.security.Principal;
-
-
-@Controller
-public class AdminController {
-
-    private final UserService userService;
-    private final RoleService roleService;
-
-    @Autowired
-    public AdminController(UserService userService, RoleService roleService) {
-        this.userService = userService;
-        this.roleService = roleService;
-    }
-
-    @GetMapping("/admin")
-    public String showAllUsers(Model model, Principal principal) {
-        User principalUser = userService.findByName(principal.getName());
-        model.addAttribute("principalUser", principalUser);
-        return "admin";
-    }
-
-    @GetMapping("/user")
-    public String showUser(Model model, Principal principal) {
-        User principalUser = userService.findByName(principal.getName());
-        model.addAttribute("principalUser", principalUser);
-        return "user";
-    }
-}
+//package com.example.demo.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//import com.example.demo.model.User;
+//import com.example.demo.service.RoleService;
+//import com.example.demo.service.UserService;
+//
+//import java.security.Principal;
+//
+//
+//@Controller
+//public class AdminController {
+//
+//    private final UserService userService;
+//    private final RoleService roleService;
+//
+//    @Autowired
+//    public AdminController(UserService userService, RoleService roleService) {
+//        this.userService = userService;
+//        this.roleService = roleService;
+//    }
+//
+//    @GetMapping("/admin")
+//    public String showAllUsers(Model model, Principal principal) {
+//        User principalUser = userService.findByName(principal.getName());
+//        model.addAttribute("principalUser", principalUser);
+//        return "admin";
+//    }
+//
+//    @GetMapping("/user")
+//    public String showUser(Model model, Principal principal) {
+//        User principalUser = userService.findByName(principal.getName());
+//        model.addAttribute("principalUser", principalUser);
+//        return "user";
+//    }
+//}
 
